99f8adbe6ec4b480818f6c3fda9a900652a88694
MilanVlaski <milan.vlaski321@gmail.com>
2023-07-08 17:33:03 +0200

    uklonjeni zakomentarisan kod iz maina

--- PRS-code/src/main/Main.java
+++ PRS-code/src/main/Main.java
@@ -1,50 +1,17 @@
 package main;
 
-import algorithms.MergeSort;
-import appsInterface.AppsFrame;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import algorithms.BubbleSort;
-import algorithms.QuickSort;
-import algorithms.Sort;
+import appsInterface.AppsFrame;
 import model.Algoritam;
 import model.SortModel;
-import parallel.ParallelMergeSort;
-import parallel.ParallelQuickSort;
 
 public class Main {
 	public static void main(String[] args) {
-
-		/*
-		 * long startTime; long endTime;
-		 * 
-		 * Integer[] niz={3,7,0,2,44,-23,8,1,14,2,-8,9,5,6,4,3,1};
-		 */
-
-		AppsFrame frame = new AppsFrame();
-
-		/*
-		 * SortModel sortModel = new SortModel(); BubbleSort bubbleSort = new
-		 * BubbleSort();
-		 * 
-		 * System.out.println(sortModel.demonstrirajSortiranje(Algoritam.MERGE, true,
-		 * 10000, false));
-		 * System.out.println(sortModel.demonstrirajSortiranje(Algoritam.BUBBLE, true,
-		 * 10000, true));
-		 * System.out.println(sortModel.demonstrirajSortiranje(Algoritam.QUICK, true,
-		 * 10000, false));
-		 * System.out.println(sortModel.demonstrirajSortiranje(Algoritam.INSERTION,
-		 * true, 10000, true));
-		 * 
-		 * System.out.println("\nBUBBLE SORT\nNiz prije sortiranja:"+prikaz(niz));
-		 * bubbleSort.sort(niz, 0, niz.length-1);
-		 * System.out.println("Niz nakon sortiranja:" + prikaz(niz));
-		 */
-
+		new AppsFrame();
 	}
 
 	public static void ispis(int[] niz) {
